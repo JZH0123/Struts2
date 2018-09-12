991ab416eb8938ea8c803715ebbeb65d6b35349e
姜 <@DESKTOP-3N8SGI8>
2018-09-12 18:41:49 +0800

    Struts登陆拦截器的使用

---
+++ SSH02/src/action/TimerAction.java
@@ -0,0 +1,14 @@
+package action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TimerAction extends ActionSupport {
+
+    public String count() {
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return SUCCESS;
+    }
+}
